2b94980e8722e9ca1593e25a9f23c181e4a31ea5
NeoTryn <114293865+NeoTryn@users.noreply.github.com>
2024-04-26 11:39:10 +0200

    can send data now

--- index.js
+++ index.js
@@ -12,7 +12,30 @@ function fillTables(data) {
         list.innerHTML += `
         <li>${user.name}</li>
         <li>${user.email}</li>
-        <li>${user.password}</li>
-        `
+        <li>${user.password}</li>`
     });
 }
+
+const data = {
+    name : "Hitler, Adolf",
+    email : "wolfschanze88@gmail.com",
+    password : "Führer88"
+};
+
+const options = {
+    method: 'POST', // or 'PUT', 'DELETE', etc.
+    headers: {
+      'Content-Type': 'application/json' // specify the content type as JSON
+    },
+    body: JSON.stringify(data) // convert the data to JSON string
+  }
+
+fetch(url, options)
+.then(response => {
+    if (!response.ok) {
+        console.log("Response not ok");
+    }
+    return response.json();
+})
+.then(data => console.log("data sent successfully"))
+.catch(error => console.log("Error sending data"));
