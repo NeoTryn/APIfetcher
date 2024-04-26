612719e271d905d1ad92d33ac32240cb30ea344a
NeoTryn <114293865+NeoTryn@users.noreply.github.com>
2024-04-26 12:23:55 +0200

    delete not working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let url = 'http://localhost:3000/users';
+let url = 'http://localhost:3000/users/';
 
 fetch(url)
 .then(response => response.json())
@@ -16,26 +16,79 @@ function fillTables(data) {
     });
 }
 
-const data = {
-    name : "Hitler, Adolf",
-    email : "wolfschanze88@gmail.com",
-    password : "Führer88"
-};
+const sendButton = document.getElementById("signUp");
+const deleteButton = document.getElementById("deleteButton")
 
-const options = {
-    method: 'POST', // or 'PUT', 'DELETE', etc.
-    headers: {
-      'Content-Type': 'application/json' // specify the content type as JSON
-    },
-    body: JSON.stringify(data) // convert the data to JSON string
-  }
+sendButton.addEventListener("click", () => {
 
-fetch(url, options)
-.then(response => {
+    let data = {
+        name,
+        email,
+        password
+    };
+
+    data.name = document.getElementById("username").value;
+    data.email = document.getElementById("email").value;
+    data.password = document.getElementById("password").value;
+
+    console.log(data);
+
+    const options = {
+        method: 'POST', // or 'PUT', 'DELETE', etc.
+        headers: {
+          'Content-Type': 'application/json' // specify the content type as JSON
+        },
+        body: JSON.stringify(data) // convert the data to JSON string
+    }
+
+    fetch(url, options)
+    .then(response => {
     if (!response.ok) {
         console.log("Response not ok");
     }
     return response.json();
+    })
+    .then(data => console.log("data sent successfully"))
+    .catch(error => console.log("Error sending data"));
 })
-.then(data => console.log("data sent successfully"))
-.catch(error => console.log("Error sending data"));
+
+deleteButton.addEventListener("click", () => {
+
+    let inGivenData = {
+        name : document.getElementById("username").value,
+        email : document.getElementById("email").value,
+        password : document.getElementById("password").value
+    }
+
+    let id = "";
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => id = checkIfUserExists(data, inGivenData))
+    .catch(error => console.log("ERROR: " + error));
+
+    let newUrl = url + id;
+
+    const options = {
+        method : 'DELETE',
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    }
+
+    fetch(newUrl,options)
+    .then(response => {
+        if (!response.ok) console.log("Response not ok");
+        return response.json();
+    })
+    .then(data => console.log("user " + data.name + " deleted successfully"))
+    .catch(error => console.log("an error occured: " + error));
+});
+
+function checkIfUserExists(data, inGivenData) {
+    data.forEach(user => {
+        if (user.name == inGivenData.name && user.email == inGivenData.email && user.password == inGivenData.password) {
+            return user._id;
+        }
+    });
+}
